afe1013f1b3fcebf3e263f186bba6875134b0dcd
sujaritchai, gayvalin <tammygayvalin@gmail.com>
2023-08-02 11:10:46 -0700

    icons implemented + working on main page

---
+++ JobIt/components/home/welcome/Welcome.jsx
@@ -0,0 +1,24 @@
+import { useState} from 'react'
+import { View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native';
+import { useRouter } from 'expo-router';
+import {icons, SIZES} from '../../../constants'
+
+import styles from './welcome.style'
+
+const Welcome = () => {
+  const router = useRouter();
+  
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  )
+}
+
+export default Welcome
---
+++ JobIt/constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/pic.png";
+
+export default {
+  profile,
+};
