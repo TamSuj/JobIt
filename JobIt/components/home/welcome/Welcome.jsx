d0add5c2199d1361e60921323b52884114302d06
sujaritchai, gayvalin <tammygayvalin@gmail.com>
2023-08-02 12:31:41 -0700

    Implementing Welcome.jsx

--- JobIt/components/home/welcome/Welcome.jsx
+++ JobIt/components/home/welcome/Welcome.jsx
@@ -13,11 +13,25 @@ import styles from './welcome.style'
 
 const Welcome = () => {
   const router = useRouter();
-  
+
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello, Tammy</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>  
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=""
+            onChange={() => {}} // To be implemented
+            placeholder="What are you looking for?"
+          />
+        </View>
+      </View>
+    </View> 
   )
 }
 
